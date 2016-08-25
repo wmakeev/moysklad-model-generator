7ae1d08981ad779b1ac0d2c7b47d6f633ae49ed4
Vitaliy Makeev <w.makeev@gmail.com>
2016-08-25 14:46:37 +0500

    Добавление преобразования normalize
    Нормализация модели для упрощения процедуры сравнения разных версий объектов модели

--- src/patches/index.js
+++ src/patches/index.js
@@ -2,5 +2,6 @@ module.exports = [
   require('./uuid'),
   require('./collection'),
   require('./error'),
-  require('./collection-type-elems')
+  require('./collection-type-elems'),
+  require('./normalize')
 ]
---
+++ src/patches/normalize/index.js
@@ -0,0 +1,30 @@
+var stringify = require('json-stable-stringify')
+var sortBy = require('lodash.sortby')
+
+/**
+ * Возвращает имя элемента
+ * @param {Object} obj Элемент
+ * @returns {string|null} Имя элемента
+ */
+function getName (obj) {
+  return obj.name || obj.elementName || obj.localName
+}
+
+/**
+ * Сортируем массивы элементов по имени
+ * @param key
+ * @param value
+ */
+function replacer (key, value) {
+  if (value instanceof Array && value.length && getName(value[0])) {
+    return sortBy(value, getName)
+  } else {
+    return value
+  }
+}
+
+module.exports = function normalize (model) {
+  return JSON.parse(stringify(model, {
+    replacer: replacer
+  }))
+}
